6c92b74a029e41a6097348a2680f9f409864b693
connsimple <rsvbreezi@gmail.com>
2025-08-17 17:36:58 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Highlight the current nav item automatically
-(function(){
-  var path = location.pathname.split('/').pop() || 'index.html';
-  document.querySelectorAll('.nav a').forEach(function(a){
-    var href = a.getAttribute('href');
-    if(href === path) a.classList.add('active');
-  });
-})();
